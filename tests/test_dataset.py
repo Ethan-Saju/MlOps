64feef17bedf507975752ff595e66a8b958481d2
EthanSaju <ethansaju10@gmail.com>
2025-11-17 00:54:45 +0530

    refactor all tests to single file

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -1,8 +1,15 @@
 from src.load_dataset import load_dataset
 
+df = load_dataset()
+
 #required columns are there 
 def test_dataset_columns():
-    df=load_dataset()
     required_columns=['Id', 'SepalLengthCm', 'SepalWidthCm', 'PetalLengthCm', 'PetalWidthCm','Species']
     for col in required_columns:
         assert col in df.columns, f"Missing column: {col}"
+def test_null_values():
+    has_null = df.isnull().values.any()
+    assert (not has_null)
+   
+def test_no_duplicates():
+    assert df.duplicated().sum() == 0
--- tests/test_null_values.py
+++
@@ -1,7 +0,0 @@
-from src.load_dataset import load_dataset
-
-def test_null_values():
-    df = load_dataset()
-    has_null = df.isnull().values.any()
-    assert (not has_null)
-   
